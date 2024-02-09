73f06501d3b4f3f7c93573a3691989883fce77a3
bagel897 <ellenagarwal897@gmail.com>
2024-02-09 10:56:30 -0600

    Undefine relation

--- backend/entity/src/user.rs
+++ backend/entity/src/user.rs
@@ -14,19 +14,23 @@ pub struct Model {
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
 pub enum Relation {
-    #[sea_orm(has_many = "Entity", from = "Column::Friends", to = "Column::Id,")]
-    SelfReferencing,
+    // #[sea_orm(
+    //     has_many = "Entity",
+    //     from = "Id::Relation::Column::Friends",
+    //     to = "Column::Id,"
+    // )]
+    // SelfReferencing,
 }
-pub struct SelfReferencingLink;
-
-impl Linked for SelfReferencingLink {
-    type FromEntity = Entity;
-
-    type ToEntity = Entity;
-
-    fn link(&self) -> Vec<sea_orm::LinkDef> {
-        vec![Relation::SelfReferencing.def()]
-    }
-}
-
+// pub struct SelfReferencingLink;
+//
+// impl Linked for SelfReferencingLink {
+//     type FromEntity = Entity;
+//
+//     type ToEntity = Entity;
+//
+//     fn link(&self) -> Vec<sea_orm::LinkDef> {
+//         Entity::has_many(Relation::SelfReferencing).into()
+//     }
+// }
+//
 impl ActiveModelBehavior for ActiveModel {}
