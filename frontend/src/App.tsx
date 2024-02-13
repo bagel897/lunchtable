1465106dbfe46064205f48c4f7fdf4c5560fa8c5
Tara Roshan <troshan@utexas.edu>
2024-02-13 12:38:28 -0600

    feat: Query status from Graphql
    Need to setup user auth and creation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,24 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { useState } from "react";
+import { gql, useQuery } from "@apollo/client";
 
-function App() {
+const GET_STATUS_QUERY = gql(/* GraphQL */ `
+  query GetStatus($user: Uuid!) {
+    getStatus(user: $user) {
+      kind
+    }
+  }
+`);
+const USER = "7f47f29f-0140-44f0-8d7f-b08779a09f8b";
+function BusyIndicator() {
+  const { loading, data } = useQuery(
+    GET_STATUS_QUERY,
+
+    // variables are also typed!
+
+    { variables: { user: USER } },
+  );
   const [status, setStatus] = useState("free");
 
   function toggleStatus() {
@@ -14,7 +30,29 @@ function App() {
       setStatus("free");
     }
   }
-
+  return (
+    <div className="card">
+      <h3>Status</h3>
+      {loading ? (
+        <p>Loading ...</p>
+      ) : (
+        <div>
+          <button onClick={() => toggleStatus()}>
+            status is
+            {data &&
+              data.kind.map((status: any) => {
+                String(status);
+              })}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
+function App() {
   return (
     <>
       <div>
@@ -25,13 +63,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Lunchtable</h1>
-      <div className="card">
-        <button onClick={() => toggleStatus()}>status is {status}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <BusyIndicator />
       </div>
+      <h1>Lunchtable</h1>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'https://flyby-router-demo.herokuapp.com/',
+  uri: "http://127.0.0.1:8000/graphql",
   cache: new InMemoryCache(),
 });
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    ,
+  </ApolloProvider>,
+);
